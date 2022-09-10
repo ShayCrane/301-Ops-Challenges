9ce7fb5d4bfad0c14bab19c97e13c5c6bcafcd26
shannon <shannon@ubuntudev>
2022-09-10 05:23:33 +0000

    adds python script written for class 09 to the repo

---
+++ ops-301d5-challenge-09.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Script Name: Bash in Python
+# Author: Shay Crane
+# Date of last revision: 09/06/2022
+# Description of purpose: PYthon Collections, Working with Lists
+
+
+
+list = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"]
+
+print("printing entire list...")
+
+print(list)
+
+print("printing fourth element of the list...")
+
+print(list[3])
+
+print("printing the sixth - the tenth element of the list...")
+
+print(list[7:12])
+
+print("changing value of the seventh element to onion...")
+
+list[5] = "onion" 
+
+print("printing updated list...")
+
+print(list)
+
+
+
