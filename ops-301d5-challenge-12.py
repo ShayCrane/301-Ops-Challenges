8a1cfb1380ff0aa8656668df4c2132bacaee8052
shannon <shannon@ubuntudev>
2022-09-15 07:00:18 +0000

    adds class 12 OC to repo

---
+++ ops-301d5-challenge-12.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Script Name: Psutil
+# Author: Shay Crane
+# Date of last revision: 09/14/2022
+# Purpose: creates a Python script that fetches various process and system information using Psutil
+
+import psutil 
+# the following commands are examples from lecture today
+print(psutil.cpu_count())
+print(psutil.cpu_percent())
+# Create a Python script that fetches this information using Psutil:
+
+# Time spent by normal processes executing in user mode
+print(psutil.cpu_times())
+
+
+# Time spent by processes executing in kernel mode
+# Time when system was idle
+# Time spent by priority processes executing in user mode
+# Time spent waiting for I/O to complete.
+# Time spent for servicing hardware interrupts
+# Time spent for servicing software interrupts
+# Time spent by other operating systems running in a virtualized environment
+# Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
