f4a754bcae702bafca3b377f3f2270ccdbe5c392
shannon <shannon@ubuntudev>
2022-09-25 03:15:55 +0000

    minor revisions to numbers recalled from list

--- ops-301d5-challenge-09.py
+++ ops-301d5-challenge-09.py
@@ -6,7 +6,7 @@
 # Description of purpose: PYthon Collections, Working with Lists
 
 
-
+#          0         1        2         3        4        5         6         7         8        9
 list = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"]
 
 print("printing entire list...")
@@ -17,13 +17,13 @@ print("printing fourth element of the list...")
 
 print(list[3])
 
-print("printing the sixth - the tenth element of the list...")
+print("printing the sixth through the tenth element of the list...")
 
-print(list[7:12])
+print(list[5:9])
 
 print("changing value of the seventh element to onion...")
 
-list[5] = "onion" 
+list[6] = "onion" 
 
 print("printing updated list...")
 
